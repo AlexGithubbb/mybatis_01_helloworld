1f6ea0a8551cbba3b40d705c3969d75e27a8a982
Alex Yao <yaobowei333@gmail.com>
2020-10-03 10:33:39 -0400

    S34-activate lazy loading on demand (case2)

--- src/test/java/MybatisTest.java
+++ src/test/java/MybatisTest.java
@@ -64,7 +64,7 @@ public class MybatisTest {
         try{
             EmployeeMapperPlus mapper = sqlSession.getMapper(EmployeeMapperPlus.class);
             Employee empById = mapper.getEmpByIdSteps(3);
-            System.out.println(empById.getLastName());
+            System.out.println(empById);
 //            System.out.println(empById.getDepartment());
         }finally {
             sqlSession.close();
